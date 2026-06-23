e9b55c1431b3ed7a43fc5fbd8d4f0b3814d6e7cd
ASIL1012 <135244927+ASIL1012@users.noreply.github.com>
2026-06-23 05:51:08 +0500

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.SUPABASE_CONFIG = {
-    URL: "https://oxuwgecwbtvinvdjqvrs.supabase.co",
-    ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dXdnZWN3YnR2aW52ZGpxdnJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNjA0NzcsImV4cCI6MjA5NjgzNjQ3N30.SyY2peC5zE2ZzYAHahOUoKkVhDWkOkUEI5fV2M084OY"
-};
